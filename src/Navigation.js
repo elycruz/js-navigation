26d089327f5741f23470a5466ad58073f266edd5
Ely De La Cruz <elycruz@elycruz.com>
2018-04-27 10:34:15 -0400

    prop-change-dispatch - Added prop change dispatching from 'Page' objects (needed for notifying parent to update their sorting when 'pages' property is called (should/will return sorted by 'order' pages).

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,14 +1,17 @@
+/**
+ * @module jsNavigation.Navigation
+ */
+
 import {partition} from 'fjl';
+import {defineEnumProp$} from 'fjl-mutable';
 import MvcPage from './MvcPage';
 import UriPage from './UriPage';
 import Page, {isPage} from './Page';
-import {PAGES_SET_INTERNAL, UUID, UUID_SET} from './Symbols';
+import {PAGES_SET_INTERNAL} from './Symbols';
 
 const
 
-    getPageSetFor = container => container[PAGES_SET_INTERNAL],
-
-    getUuidSetFor = container => container[UUID_SET]
+    getPageSetFor = container => container[PAGES_SET_INTERNAL]
 
 ;
 
@@ -21,28 +24,44 @@ export const
     addPage = (page, container) => {
         const _page = normalizePage(page);
         getPageSetFor(container).add(_page);
-        getUuidSetFor(container).add(_page[UUID]);
         return [_page, container];
     },
 
     addPages = (pages, container) => {
-        if (pages) {
-            container.pages = pages.map(page => {
-                if (isPage(page)) {
-                    if (page.pages) {
-                        return addPages(page.pages, page);
-                    }
-                    return page;
-                }
-                const [_page] = addPage(page, container);
-                if (_page.pages) {
-                    addPages(_page.pages, _page);
-                }
-                return _page;
-            });
+        if (!pages) {
+            return container;
         }
-        return container;
+
+        container.pages = pages.map(page => {
+            const _isPage = isPage(page);
+            if (_isPage && hasPage(page, container)) {
+                return page;
+            }
+
+            if (_isPage) {
+                if (!page.parent) {
+                    page.parent = container;
+                }
+                if (page.pages) {
+                    return addPages(page.pages, page);
+                }
+                return page;
+            }
+
+            const [_page] = addPage(page, container);
+
+            _page.parent = container;
+
+            if (_page.pages) {
+                addPages(_page.pages, _page);
+            }
+
+            return _page;
+        });
     },
+
+    hasPage = (page, container) =>
+        findPageBy(_page => _page === page, container),
 
     removePage = (page, container) => {
         const s = getPageSetFor(container);
@@ -63,7 +82,8 @@ export const
     removePagesBy = (pred, container) => {
         const s = getPageSetFor(container),
             partitioned = partition(pred, Array.from(s.values()))[1];
-        s.clear().add(...partitioned);
+        s.clear();
+        partitioned.forEach(x => s.add(x));
         return container;
     },
 
@@ -95,6 +115,7 @@ export const
 export default class Navigation extends Page {
     constructor (pages, props) {
         super(props);
+        defineEnumProp$(Boolean, 'needsOrdering', false, [this]);
         Object.defineProperty(this, 'size', {
             get: function () { return this[PAGES_SET_INTERNAL].size; },
             enumerable: true
@@ -102,37 +123,8 @@ export default class Navigation extends Page {
         addPages(pages, this);
     }
 
-    addPage (page) {
-        addPage(page, this);
-        return this;
-    }
-
-    addPages (pages) {
-       return addPages(pages, this);
-    }
-
-    findPageBy (pred) {
-        return findPageBy(pred, this);
-    }
-
-    findPagesBy (pred) {
-        return findPagesBy(pred, this);
-    }
-
-    findPagesByProp (key, value) {
-        return findPagesByProp(key, value, this);
-    }
-
-    removePage (pageName) {
-        return removePage(pageName, this);
-    }
-
-    removePages (pages) {
-        return removePages(pages, this);
-    }
-
-    removePagesBy (pred) {
-        return removePagesBy(pred, this);
+    setNeedsOrdering () {
+        this.needsOrdering = true;
     }
 
 }
--- src/Page.js
+++ src/Page.js
@@ -1,33 +1,69 @@
-import {defineEnumProps} from 'fjl-mutable';
+import {defineEnumProps, errorIfNotTypeOnTarget$} from 'fjl-mutable';
 import {assignDeep} from 'fjl';
 import {PAGES_SET_INTERNAL, UUID, UUID_SET} from './Symbols';
 import {uuid} from './utils';
 
-export const isPage = x => x instanceof Page;
+export const
+
+    isPage = x => x instanceof Page,
+
+    UUID_AND_ORDER_MAP = Symbol('uuid_and_order_map')
+
+;
 
 export default class Page {
-    static isPage (x) { return isPage(x); }
     constructor (props) {
+        let _order;
         defineEnumProps([
             [String, 'label'],
             [String, 'fragment'],
             [Object, 'htmlAttribs'],
-            [Number, 'order'],
             [String, 'resource'],
             [String, 'privilege'],
             [Boolean,'active'],
             [Boolean,'visible'],
             [String, 'type'],
-            [Array, 'pages']
+            [Array, 'pages'],
+            [Page, 'parent']
         ], this);
-        assignDeep(props);
 
-        // Set privates
+        this.propChanged = () => (undefined);
+
         Object.defineProperties(this, {
+            order: {
+                get: () => _order,
+                set: x => {
+                    errorIfNotTypeOnTarget$(Number, this, 'order', x);
+                    _order = x;
+                    this.dispatchPropChanged('order');
+                },
+                enumerable: true
+            },
             [UUID]: {value: uuid()},
             [PAGES_SET_INTERNAL]: {value: new Set()}, // Set for storing a unique set of add pages.
-            [UUID_SET]: {value: new Set()}            // Set for storing fast list for searching for pages.
+            [UUID_SET]: {value: new Set()},            // Set for storing fast list for searching for pages.
+            [UUID_AND_ORDER_MAP]: {value: new Map()}            // Set for storing fast list for searching for pages.
         });
 
+        assignDeep(props);
     }
+
+    onPropChanged (e) {
+        if (e.detail.propName === 'order' && this.parent) {
+            if (this.parent.setNeedsOrdering) {
+                this.parent.setNeedsOrdering();
+            }
+        }
+        this.propChanged(e);
+    }
+
+    dispatchPropChanged (propName) {
+        this.onPropChanged({
+            detail: {
+                propName: propName,
+                propValue: this[propName]
+            }
+        });
+    }
+
 }

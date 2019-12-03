f6a67af9eba4135cbcf1da00b1168d31a0178926
Ely De La Cruz <elycruz@elycruz.com>
2019-12-02 20:14:12 -0500

    copied types to './src/types'.

---
+++ src/types.ts
@@ -0,0 +1,73 @@
+export interface UriPageLike {
+    uri?: string;
+}
+
+export interface MvcPageLike {
+    action?: string;
+    controller?: string;
+    params?: object;
+    route?: object;
+    routeMatch?: object;
+    useRouteMatch?: boolean;
+    router?: object;
+}
+
+export interface PageLike extends UriPageLike, MvcPageLike {
+    type?: string;
+    active?: boolean;
+    order?: number;
+    parent?: PageLike;
+    label?: string;
+    fragment?: string;
+    htmlAttribs?: object;
+    resource?: string;
+    privilege?: string;
+    visible?: boolean;
+    pages?: PageLike[];
+}
+
+export interface PageShape extends PageLike {
+    parent: PageShape;
+    requiresOrdering: boolean;
+    requiresActivityEvaluation: boolean;
+
+    orderChanged(): void;
+
+    activeChanged(): void;
+
+    pages: PageShape[];
+    readonly size: number;
+}
+
+export interface PageConstructor extends PageShape {
+    // PagePropsLike props
+    active: boolean;
+    parent: PageShape;
+    order: number;
+    visible: boolean;
+
+    type?: string;
+    label?: string;
+    fragment?: string;
+    htmlAttribs?: object;
+    resource?: string;
+    privilege?: string;
+
+    // PageShape props
+    requiresOrdering: boolean;
+    requiresActivityEvaluation: boolean;
+    pages: PageShape[];
+    readonly size: number;
+
+    constructor(props?: PageLike);
+
+    orderChanged(): void;
+
+    activeChanged(): void;
+}
+
+export interface DerivedPageShape extends PageShape {
+    readonly type: string;
+}
+
+export  type FilterPred = (any, number?, array?) => boolean;
